82760f5ce330f3dbad43a14c480993a5e229729a
dhanushka <dhanushka.sampath@madmobile.com>
2024-02-05 15:42:51 +0530

    first commit

---
+++ src/main/java/com/learning/searchingtechniques/SearchingTechniquesApplication.java
@@ -0,0 +1,13 @@
+package com.learning.searchingtechniques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchingTechniquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchingTechniquesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/searchingtechniques/SearchingTechniquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.searchingtechniques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchingTechniquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
